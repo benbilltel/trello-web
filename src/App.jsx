e0b000a46fc8bb23478fa6f5c44616fe0e583e5c
Ben <benbilldev@gmail.com>
2024-02-21 18:28:12 +0700

    Set up dark-light mode UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import Button from '@mui/material/Button'
 import './App.css'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import { useColorScheme } from '@mui/material/styles'
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-mode"
+        id="select-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <Box sx={{ display: 'flex', alignItems:'center', gap:1 }}>
+            <LightModeIcon fontSize='small'></LightModeIcon>
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems:'center', gap:1 }}>
+            <DarkModeIcon fontSize='small'></DarkModeIcon>
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display: 'flex', alignItems:'center', gap:1 }}>
+            <SettingsBrightnessIcon fontSize='small'></SettingsBrightnessIcon>
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function App() {
   return (
     <>
-      <Button variant="contained">Hello world</Button>
-      <ThreeDRotation/>
-      <div>Test</div>
+      <ModeSelect></ModeSelect>
     </>
   )
 }
 
-export default App;
+export default App
